e4a6d2a1f2f6a9a3f349ff8731894ee129080ced
Nemsog <90186906+Nemsog@users.noreply.github.com>
2021-12-19 23:38:25 +0100

    Update

--- src/themes/default.js
+++ src/themes/default.js
@@ -7,9 +7,9 @@ export default {
   // Colors for layout
   colors: {
     primary1: "hsl(204,23.8%,95.9%)",
-    background1: "#0F1624",
+    background1: "#191919",
     accent1: "hsl(34.9,98.6%,72.9%)",
-    button: "hsl(205.1,100%,36.1%)",
+    button: "linear-gradient(270deg, rgba(252,252,252,1) 0%, rgba(115,244,112,1) 50%, rgba(52,255,0,1) 100%);",
     background2: "hsl(232.7,27.3%,23.7%)",
   },
   // Breakpoints for responsive design
